07a93a6e08284718e9719b5ed4994804548618f1
Diego Alonso Chavez Escalante <diescala@amazon.com>
2021-03-01 00:38:49 -0600

    Setting dummy function.

--- src/gnomo_lambdas/handlers/report_tenant_handler.py
+++ src/gnomo_lambdas/handlers/report_tenant_handler.py
@@ -1,19 +1,12 @@
 import json
-from random import seed, random
 
-seed(1)
 
 def lambda_handler(event, context):
-
-    parameters = event.get('queryStringParameters', {})
-    doc_id = parameters.get('docID', parameters.get('dni', None))
-    score = random() if doc_id is not None else None
-
     print(event)
+    print('VAMOOOOOO')
 
     dummy_score = {
-        'docId': doc_id,
-        'score': score
+        'message': 'Sape'
     }
 
     return {
