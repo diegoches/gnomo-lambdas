118b6dd802d3611ac1d74b53ab05aa1b5c05f3ce
Diego Alonso Chavez Escalante <diescala@amazon.com>
2021-03-11 04:47:28 -0600

    Adding decimal encoder.

--- src/gnomo_lambdas/helpers/decimal_encoder.py
+++ src/gnomo_lambdas/helpers/decimal_encoder.py
@@ -5,8 +5,5 @@ from json import JSONEncoder
 class DecimalEncoder(JSONEncoder):
     def default(self, o):
         if isinstance(o, Decimal):
-            # wanted a simple yield str(o) in the next line,
-            # but that would mean a yield on the line with super(...),
-            # which wouldn't work (see my comment below), so...
             return (str(o) for o in [o])
         return super(DecimalEncoder, self).default(o)
